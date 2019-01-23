7b65701b5ce25446aefa05476e536fea82f52b2e
trupinguillaume <gtrupin@gmail.com>
2019-01-23 14:34:16 +0100

    refact architecture

---
+++ lib/path.rb
@@ -0,0 +1,16 @@
+class Path
+  attr_reader :segments
+
+  def initialize(segments:)
+    @segments = segments
+    freeze
+  end
+
+  def length
+    if @segments.count > 0
+      @segments.map { |segment| segment.distance }.reduce(&:+)
+    else
+      0
+    end
+  end
+end
--- spec/segment_spec.rb
+++ spec/segment_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'segment'
+require 'point'
 
 RSpec.describe "Ruby kata" do
   context "A Segment" do
@@ -31,32 +32,4 @@ RSpec.describe "Ruby kata" do
       end
     end
   end
-
-  context "A Path" do
-    context "wich is empty" do
-      it 'should render length' do
-        length = Path.new(segments: []).length
-        expect(length).to eq 0
-      end
-    end
-
-    context "wich contains a single segment" do
-      it 'should render length' do
-        segment = Segment.new(from: Point.new(x: 0, y: 0), to: Point.new(x: 1, y: 0))
-        length = Path.new(segments: [segment]).length
-        expect(length).to eq 1
-      end
-    end
-
-    context "wich contains two segments" do
-      it 'should render length' do
-        segment_1 = Segment.new(from: Point.new(x: 2, y: 1), to: Point.new(x: 3, y: 1))
-        segment_2 = Segment.new(from: Point.new(x: 3, y: 1), to: Point.new(x: 3, y: 3))
-        length = Path.new(segments: [segment_1, segment_2]).length
-        expect(length).to eq 3
-      end
-    end
-  end
-
-
 end
