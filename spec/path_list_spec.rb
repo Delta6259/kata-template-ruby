26a0ab0fdd1bcc8f5b7507ecee78eb0451b94d18
trupinguillaume <gtrupin@gmail.com>
2019-01-25 15:11:38 +0100

    path list spec skeletton

---
+++ spec/path_list_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require 'path'
+require 'segment'
+require 'point'
+
+RSpec.describe "PathList" do
+  context "when there is no segment" do
+    it 'should return the best path' do
+
+    end
+  end
+
+  context "when there is 1 segment" do
+    it 'should return the best path' do
+    end
+  end
+
+  context "when there is no segment circle" do
+    it 'should return the best path' do
+    end
+  end
+
+  context "when there are segments with circles" do
+    it 'should return the best path' do
+    end
+  end
+end
