c8d54d3ed4bd30bcff23e30a10def2ff1cd2b938
trupinguillaume <gtrupin@gmail.com>
2019-01-25 16:04:39 +0100

    refact

--- spec/path_list_spec.rb
+++ spec/path_list_spec.rb
@@ -7,24 +7,27 @@ require 'path_factory'
 RSpec.describe "PathList" do
 
   context "when there is no segment" do
-    it 'should not return the best path' do
+    it 'should not return all possible paths' do
       path_list = PathFactory.new(from: Point.new(x: 0, y: 0), to: Point.new(x: 1, y: 0))
       expect(path_list.all_possible_paths(segments: nil)).to eq []
     end
   end
 
   context "when there is 1 segment" do
-    it 'should return the best path' do
+    it 'should return all possible paths' do
+      path_list = PathFactory.new(from: Point.new(x: 1, y: 1), to: Point.new(x: 2, y: 2))
+      segments = [Segment.new(from: Point.new(x: 1, y: 1), to: Point.new(x: 2, y: 2))]
+      expect(path_list.all_possible_paths(segments: segments)).to eq anything
     end
   end
 
   context "when there is no segment circle" do
-    it 'should return the best path' do
+    it 'should return all possible paths' do
     end
   end
 
   context "when there are segments with circles" do
-    it 'should return the best path' do
+    it 'should return all possible paths' do
     end
   end
 end
