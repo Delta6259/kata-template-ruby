6835a30240494bfee6157b55cf83c233a9de71c9
trupinguillaume <gtrupin@gmail.com>
2019-01-30 17:29:50 +0100

    Initialize new class

--- lib/path_factory.rb
+++ lib/path_factory.rb
@@ -1,27 +1,26 @@
 class PathFactory
 
-  def initialize(from:, to:)
+  def initialize(from:, to:, segments:)
     @from = from
     @to = to
+    @segments = segments
   end
 
-  def all_possible_paths(segments:)
-    if segments == nil || segments == []
+
+  def possible_paths(starting_point, ending_point, segments, valid_paths = [])
+    if segments.length == 0 && valid_paths.length == 0
       []
     else
-
-
-
+      case segment[0].from
+      when starting_point
+        # On commence une nouvelle branche depuis le départ
+        if segment[0].to == ending_point
+          possible_paths(starting_point, ending_point, segments.drop(1), valid_paths << Path.new(segments: segments[0]))
+        end
+      else
+        # On
+      end
     end
   end
 
-  private
-
-  def make_path(path_list)
-    if path_list.length == 0
-      0
-    else
-      head of array + sum_array(tail of array)
-    end
-  end
 end
